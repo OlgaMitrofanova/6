c45ea75b3abf6167284c38a94eeefd38c45d0bc8
Olga <chainikova@gmail.com>
2023-03-09 17:28:19 +0300

    5

---
+++ src/test/java/org/example/lesson5/GetCategoryTest.java
@@ -0,0 +1,38 @@
+package org.example.lesson5;
+
+import lesson5.api.CategoryService;
+import lesson5.api.RetroUtils;
+import lesson5.dto.GetCategoryResponse;
+import lesson5.utils.RetrofitUtils;
+import lombok.SneakyThrows;
+import org.hamcrest.CoreMatchers;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+public class GetCategoryTest {
+
+    static CategoryService categoryService;
+    @BeforeAll
+    static void beforeAll() {
+        categoryService = RetrofitUtils.getRetrofit().create(CategoryService.class);
+    }
+
+    @SneakyThrows
+    @Test
+    void getCategoryByIdPositiveTest() {
+        Response<GetCategoryResponse> response = categoryService.getCategory(1).execute();
+
+        assertThat(response.isSuccessful(), CoreMatchers.is(true));
+        assertThat(response.body().getId(), equalTo(1));
+        assertThat(response.body().getTitle(), equalTo("Food"));
+        response.body().getProducts().forEach(product ->
+                assertThat(product.getCategoryTitle(), equalTo("Food")));
+
+
+    }
+}
